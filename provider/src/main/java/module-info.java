9cff6f5a342f64f22803c0279ab0afa69831990f
Fredrik Lundsten <f.lundsten@hotmail.se>
2024-04-18 14:41:35 +0200

    Refactor

--- provider/src/main/java/module-info.java
+++ provider/src/main/java/module-info.java
@@ -1,11 +1,9 @@
 import org.example.service.Continent;
 import org.example.service.Menu;
-import org.provider.Asia;
-import org.provider.Europe;
-import org.provider.MenuImpl;
+import org.provider.*;
 
 module provider {
     requires service;
-    provides Continent with Asia, Europe;
+    provides Continent with Asia, Europe, NorthAmerica, SouthAmerica, Africa;
     provides Menu with MenuImpl;
 }
