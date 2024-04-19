ecb1973ef289ee553947e0fa91be9e4237dc574c
Fredrik Lundsten <f.lundsten@hotmail.se>
2024-04-19 16:15:54 +0200

    Refactor to make menu depend on annotation value.

--- consumer/src/main/java/org/consumer/Main.java
+++ consumer/src/main/java/org/consumer/Main.java
@@ -1,37 +1,42 @@
 package org.consumer;
 
 import org.example.service.Continent;
+import org.example.service.Direction;
+import org.example.service.Hemisphere;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         ServiceLoader<Continent> continents = ServiceLoader.load(Continent.class);
-
-        HashSet<String> m = new HashSet<>();
-
         var listOfContinents = new ArrayList<Continent>();
         continents.forEach(listOfContinents::add);
-
         int input = 0;
         applicationLoop(input, listOfContinents);
     }
 
     private static void applicationLoop(int input, ArrayList<Continent> listOfContinents) {
+        List<Direction> directionList = getAllDirections(listOfContinents);
         while (input != -1) {
             menu(listOfContinents);
-            input = inputFromUser(listOfContinents.size());
+            input = inputFromUser(directionList.size());
             if (input == -1) {
                 break;
             }
-            var m = listOfContinents.get(input);
-            System.out.printf("""
-                    Located in %s:
-                    """, m.simpleName());
+            var chosenDirection = directionList.get(input);
 
-            listOfContinents.get(input).someCountries().forEach(System.out::println);
+            System.out.println("In the " + chosenDirection + " hemisphere we find:\n");
+            for (Continent listOfContinent : listOfContinents) {
+                Direction[] directionsValue = listOfContinent.getClass().getAnnotation(Hemisphere.class).value();
+                for (Direction direction : directionsValue) {
+                    if (direction.equals(chosenDirection)) {
+                        System.out.println(String.join(",", listOfContinent.someCountries()));
+                    }
+                }
+            }
             System.out.println("Press 'Enter' to go back..");
             scanner.nextLine();
         }
@@ -54,29 +59,34 @@ public class Main {
         }
     }
 
-    private static void menuBuilder(StringBuilder sb, int i, int size, ArrayList<Continent> listOfContinents) {
-        if (i == size)
-            return;
-        sb.append(i)
-                .append(". ")
-                .append(listOfContinents
-                        .get(i).simpleName())
-                .append("\n");
-        menuBuilder(sb, i + 1, size, listOfContinents);
+    private static void menu(ArrayList<Continent> listOfContinents) {
+        menuBuilderVG(getAllDirections(listOfContinents));
     }
 
-    private static void menu(ArrayList<Continent> listOfContinents) {
+    private static List<Direction> getAllDirections(ArrayList<Continent> listOfContinents) {
+        Set<Direction> setOfDirections = new HashSet<>();
+        for (Continent listOfContinent : listOfContinents) {
+            Direction[] directionsValue = listOfContinent.getClass().getAnnotation(Hemisphere.class).value();
+            setOfDirections.addAll(Arrays.asList(directionsValue));
+        }
+        return new ArrayList<>(setOfDirections);
+    }
+
+    private static void menuBuilderVG(List<Direction> ListOfDirections) {
         StringBuilder sb = new StringBuilder();
-        menuBuilder(sb, 0, listOfContinents.size(), listOfContinents);
+        int counter = 0;
+        for (Direction direction : ListOfDirections) {
+            sb.append(++counter).append(". ").append(direction).append("\n");
+        }
         System.out.printf("""
                 ________________________________
                 Pick a number to display
-                some countries on that continent
+                some countries located in
+                that Hemisphere.
                 ________________________________
                 %s
                 'Q'. Quit
                 ________________________________
                 %n""", sb);
-
     }
 }
